541158c530f9a99c4d4b56c42d1a28b3521e726b
João Lucas de Freitas Rodrigues <joaolucasfreitas360@gmail.com>
2021-05-05 23:39:07 -0300

    Contagem-cedulas.py

---
+++ Contagem-cedulas.py
@@ -0,0 +1,14 @@
+def din(a):
+    val = int(a)
+    notas = [200, 100, 50, 20, 10, 5, 2, 1]
+    notas.sort()
+    notas.reverse()
+    numNotas = []
+    for i in notas:
+        numNotas.append(val/i)
+        val %= i
+    for i in range(len(notas)):
+        print("Notas de %d:%d" % (notas[i], numNotas[i]))
+    return a
+val = int(input("Digite o valor:"))
+print(din(val))
